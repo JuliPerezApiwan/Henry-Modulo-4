15ca2e9c56e90e370660c3319edf1cf73c72b60a
MedicalConnect <medicalconnectar@gmail.com>
2023-05-07 21:40:45 -0300

    ok

--- 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Ability.js
+++ 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Ability.js
@@ -1,5 +1,30 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-   sequelize.define('ability', {});
-};
+   sequelize.define("Ability", {
+     name: {
+       type: DataTypes.STRING,
+       allowNull: false,
+       unique: 'compositeIndex'
+     },
+     description: {
+       type: DataTypes.TEXT
+     },
+     mana_cost: {
+       type: DataTypes.FLOAT,
+       allowNull: false,
+       unique: 'compositeIndex',
+       validate: {
+         min: 10.0,
+         max: 250.0
+       }
+     },
+     summary: {
+       type: DataTypes.VIRTUAL,
+       get(){
+         return `${this.name} (${Math.floor(this.mana_cost)} points of mana) - Description: ${this.description}`
+       }
+     }
+   });
+ };
+ 
--- 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Role.js
+++ 03-sequelize/Homework - Part 1/01 - Exercices/db/models/Role.js
@@ -1,5 +1,15 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-   sequelize.define('role', {});
-};
+   sequelize.define("Role", {
+     name: {
+       type: DataTypes.STRING,
+       unique: true,
+       allowNull: false
+     },
+     description: {
+       type: DataTypes.STRING
+     }
+   });
+ };
+ 
